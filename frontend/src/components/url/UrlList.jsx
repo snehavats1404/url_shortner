5c2349e855e7c26184643e72aaf2c14edccdf69d
snehavats1404 <snehavats987@gmail.com>
2025-01-15 22:01:37 +0530

    Update UrlList.jsx

--- frontend/src/components/url/UrlList.jsx
+++ frontend/src/components/url/UrlList.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getUserUrls } from '../../services/urlService';
 import { useAuth } from '../../contexts/AuthContext';
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://url-shortner-backend-madh.onrender.com';
 
 export default function UrlList() {
   const [urls, setUrls] = useState([]);
